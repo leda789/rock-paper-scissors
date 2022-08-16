39aed42be9a032b7fda781bc27088e9ad3eb0222
Veda Farmer <vedafarmer@hotmail.com>
2022-08-16 14:42:37 -0700

    Add boilerplate html page, pseudocode js

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+  //will randomly return RPS
+}
+
+function playRound(playerSelection, computerSelection) {
+  //compares the two selections to see who wins
+  //returns string that declares the winner
+  //but also says who won
+}
+
+function game() {
+  //loops play round 5 times
+  //keeps score
+  //reports winner or loser
+}
+
