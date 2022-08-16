765bb0120a1d8ea36f3fcf854e362b7b5f401427
Veda Farmer <vedafarmer@hotmail.com>
2022-08-16 15:35:03 -0700

    Generate a random answer, tell you who has won

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function getComputerChoice() {
-  //will randomly return RPS
+  let num = (Math.random()*1000);
+  if (num <= 333) return "rock"
+  else if (num <= 666) return "paper"
+  else return "scissors"
 }
 
 function playRound(playerSelection, computerSelection) {
-  //compares the two selections to see who wins
-  //returns string that declares the winner
-  //but also says who won
-}
+  if (computerSelection === "rock") {
+    if (playerSelection === "paper") return "Paper beats rock, you win!"
+    else if (playerSelection === "scissors") return "Rock beats scissors, you lose!"
+    else return "Both rock, tie!" }
+
+  else if (computerSelection === "paper") {
+    if (playerSelection === "paper") return "Both paper, tie!"
+    else if (playerSelection === "scissors") return "Scissors beats paper, you win!"
+    else return "Paper beats rock, you lose!" }
+
+  else { //scissors
+    if (playerSelection === "paper") return "Scissors beats paper, you lose!"
+    else if (playerSelection === "scissors") return "Both scissors, tie!"
+    else return "Rock beats scissors, you win!" }
+  }
 
 function game() {
   //loops play round 5 times
